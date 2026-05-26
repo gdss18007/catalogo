ddab6e7d382240e828ccb60061fc9542b48254bb
gdss18007 <gabriel.santossilva.18007@gmail.com>
2026-05-26 15:39:57 -0300

    Atualizacao automatica da loja: 26/05/2026 15:39:57,06

--- atualizar.bat
+++ atualizar.bat
@@ -1,17 +1,20 @@
 @echo off
-:: Navega para a pasta onde o script esta (deve ser a pasta do repositorio)
+:: Navega para a pasta onde o script esta
 cd /d "%~dp0"
 
-echo [1/3] Verificando e adicionando alteracoes (novas, deletadas, renomeadas)...
+echo [1/4] Baixando atualizacoes do GitHub (Garantindo sincronia)...
+git pull origin master --rebase
+
+echo.
+echo [2/4] Verificando e adicionando alteracoes locais...
 git add -A
 
 echo.
-echo [2/3] Criando o ponto de salvamento (commit)...
-:: O commit leva a data e a hora atual como mensagem automatica
-git commit -m "Atualizacao automatica do catalogo: %date% %time%"
+echo [3/4] Criando o ponto de salvamento (commit)...
+git commit -m "Atualizacao automatica da loja: %date% %time%"
 
 echo.
-echo [3/3] Enviando para o GitHub...
+echo [4/4] Enviando tudo de volta para o GitHub...
 git push origin master
 
 echo.
